db619dabcb9fc73d13d6680e32cbe955a0fd6f38
= <=>
2022-09-28 10:44:46 -0600

    tic-tac

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import Square from './Square';
+
+function calculateWinner (arr) {
+    const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+for (let i = 0; i < lines.length; i++){
+  const [a, b ,c] = lines[i];
+  if (arr[a] &&
+      arr[a] === arr[b] &&
+      arr[a] === arr[c]
+    ) {
+      return `${arr[a]} won!`
+  }
+}
+return 'who will win?'
+}
+
+function App() {
+
+  const speed = 'random string'
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  };
+
+  return (
+    <div>
+    
+    <div className="App">
+     <span>{calculateWinner(squares)}</span>
+    <button onClick={handleClick}>Reset</button>
+      <div className='container'>
+      {squares.map((val, index) => {
+                return (
+                    <Square
+                        setSquares={setSquares}
+                        index={index}
+                        squareValue={val}
+                        squares={squares}
+                        player={player}
+                        setPlayer={setPlayer}
+                    />
+                );
+            })}
+      </div>
+      <div className='backround'></div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
